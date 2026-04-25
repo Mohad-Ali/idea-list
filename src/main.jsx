9f8c0f666008417b087b16e9f3b2ddf1aedfa093
Mohad-Ali <mohdali02064@gmail.com>
2026-04-25 15:03:16 +0530

    path change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'   // make sure path is correct
 import { Toaster } from "react-hot-toast";
@@ -10,10 +10,10 @@ import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <Toaster position="top-right" />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
